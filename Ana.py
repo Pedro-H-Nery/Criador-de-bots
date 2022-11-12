f32ba9a7becd0c385f1ceecff7f52e33a61d2a2a
Pedro-H-Nery <pedronery404@gmail.com>
2022-11-12 01:08:54 -0300

    Adicionando a opção de uso teclado no bot

--- Ana.py
+++ Ana.py
@@ -10,21 +10,30 @@ class Ana:
         self.movimentos=[]
 
     def criarBot(self):
-        win32api.MessageBox(0, 'Clique nos lugares que deseja que o bot clique.\nNo final clique no canto inferior direito para encerrar a aprendizagem.', 'Aprendizagem do bot', 0x00001000)
+        win32api.MessageBox(0, 'Clique nos lugares que deseja que o bot clique.\nNo final clique na tecla ESC para encerrar a aprendizagemo.', 'Aprendizagem do bot', 0x00001000)
         self.movimentos=[]
-        with mouse.Listener(on_click=self.clicarMouse) as listener:
-            listener.join()
-
-        listener = mouse.Listener(on_click=self.clicarMouse)
-        listener.start()
-        listener.stop()
-        self.ativarBot(self.movimentos)
+        self.mouse_listener = mouse.Listener(on_click=self.clicarMouse, on_scroll=self.scrollMouse)
+        self.keyboard_Listener=keyboard.Listener(on_press=self.pressionarTeclado, on_release=self.soltarTecla)
+        self.mouse_listener.start()
+        self.keyboard_Listener.start()
+        self.mouse_listener.join()
+        self.keyboard_Listener.join()
 
     def clicarMouse(self, x, y, button, pressed):
-        if(x>1200 and y>600):
-            return False
         if(pressed):
             self.movimentos.append(f'Clique({x},{y})')
+
+    def scrollMouse(self,x, y, dx, dy):
+        self.movimentos.append(f'Scroll({x},{y},{dx},{dy})')
+
+    def pressionarTeclado(self, key):
+        self.movimentos.append(f'Tecla({key})')
+
+    def soltarTecla(self,key):
+        if key == keyboard.Key.esc:
+            self.mouse_listener.stop()
+            self.keyboard_Listener.stop()
+            self.ativarBot(self.movimentos)
     
     def criarComando(self):
         pass
@@ -38,6 +47,9 @@ class Ana:
             if(mov[0]=="C"):
                 time.sleep(2)
                 pyautogui.click(int(mov[7:mov.find(",")]),int(mov[mov.find(",")+1:mov.find(")")]))
+            elif(mov[0]=="T"):
+                time.sleep(2)
+                pyautogui.press(mov[7:mov.find(")")-1])
         time.sleep(1)
         win32api.MessageBox(0, 'O bot terminou sua tarefa.', 'Finalização do bot', 0x00001000)
 
